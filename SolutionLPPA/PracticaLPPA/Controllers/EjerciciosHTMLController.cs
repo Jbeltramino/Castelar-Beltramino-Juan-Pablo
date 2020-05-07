18dcf294185ed7e15c2cee7cf775242ab7f42dfc
JBeltramino <juan.beltramino@hotmail.com>
2020-05-07 17:46:42 -0300

    Se dividieron en vistas y controllers los ejercicios de HTML, y se maqueto la pagina

---
+++ SolutionLPPA/PracticaLPPA/Controllers/EjerciciosHTMLController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaLPPA.Controllers
+{
+    public class EjerciciosHTMLController : Controller
+    {
+        // GET: EjerciciosHTML
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio1()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio2()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio3()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio4()
+        {
+            return View();
+        }
+        public ActionResult Enlaces()
+        {
+            return View();
+        }
+        public ActionResult Listas()
+        {
+            return View();
+        }
+        public ActionResult Tabla1()
+        {
+            return View();
+        }
+        public ActionResult Tabla2()
+        {
+            return View();
+        }
+        public ActionResult Formularios1()
+        {
+            return View();
+        }
+        public ActionResult Formularios2()
+        {
+            return View();
+        }
+        public ActionResult FormulariosHTML5()
+        {
+            return View();
+        }
+    }
+}
